a776a276f86a967f8459d8eb29bf4f82b31b386b
Ajith Sumesh <asumesh15@users.noreply.github.com>
2021-02-23 17:03:29 -0500

    Add files via upload

--- limo.py
+++ limo.py
@@ -1,5 +1,7 @@
 from taxii2client.v20 import Server, Collection
 import json
+
+taxiiCollection = Collection('https://localhost:9000/services/collection-management-a', user='test', password='test')
 server = Server('https://limo.anomali.com/api/v1/taxii2/taxii/', user='guest', password='guest')
 api_root = server.api_roots[0]
 collections_limo = [api_root.collections[0]]#Phish Tank collection
@@ -7,5 +9,6 @@ for collection in collections_limo:
     print(collection.title)
     obj = collection.get_objects(limit=1)
     print(obj)
+    
     #with open('data.json', 'w') as outfile:
     #    json.dump(obj, outfile)
