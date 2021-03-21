6d55a5e27259369b8227750239ca4f1028ea5775
Ajith Sumesh <asumesh15@users.noreply.github.com>
2021-03-20 22:19:25 -0400

    Add files via upload

--- limo.py
+++ limo.py
@@ -1,4 +1,4 @@
-from taxii2client.v20 import Server, Collection
+from taxii2client.v20 import Server, Collection, as_pages
 import json
 from cabby import create_client
 client = create_client('localhost',discovery_path='/services/discovery-a',port=9000)
@@ -7,14 +7,15 @@ client.set_auth(username='test', password='test')
 
 server = Server('https://limo.anomali.com/api/v1/taxii2/taxii/', user='guest', password='guest')
 api_root = server.api_roots[0]
-collections_limo = [api_root.collections[0]]#Phish Tank collection
+collections_limo = [api_root.collections[1]]#collection name
 for collection in collections_limo:
     print(collection.title)
-    obj = collection.get_objects(limit=1)
+    obj = collection.get_objects()
+    obj['dataSource'] = "Anomali Limo"
+    obj['collection'] = collection.title
     objStr = json.dumps(obj)
-    print(objStr)
-    #client.push(objStr, 'urn:stix.mitre.org:json:2.1',uri='/services/inbox-a',collection_names=['collection-a'])
+    client.push(objStr, 'urn:stix.mitre.org:json:2.1',uri='/services/inbox-a',collection_names=['collection-a'])
     #print(obj)
     
-    with open('data.json', 'w') as outfile:
-        json.dump(obj, outfile)
+    #with open('data.json', 'w') as outfile:
+    #    json.dump(obj, outfile)
