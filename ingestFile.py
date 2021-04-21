126e7763be07bb69f9123181fb4bc3ce4dc2b30b
Ajith Sumesh <asumesh15@users.noreply.github.com>
2021-04-21 13:54:03 -0400

    Add files via upload

--- ingestFile.py
+++ ingestFile.py
@@ -7,33 +7,29 @@ import re
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--collection", help="Enter a collection number (0, 1, 2..)")
+parser.add_argument("--file", help="Enter a json file name (1.json, test.json, etc.)")
 args = parser.parse_args()
 
 tz = timezone('US/Eastern')
 #client = create_client('localhost',discovery_path='/services/discovery-a',port=9000)
 #client.set_auth(username='test', password='test')
 #taxiiCollection = Collection('http://localhost:9000/services/collection-management-a', user='test', password='test')
-collectionNum = int(args.collection)
-server = Server('https://limo.anomali.com/api/v1/taxii2/taxii/', user='guest', password='guest')
-api_root = server.api_roots[0]
-collections_limo = [api_root.collections[collectionNum]]#collection name
-for collection in collections_limo:
-    print(collection.title)
-    obj = collection.get_objects()
-
-    objects = obj['objects']
+fileName = args.file
+with open(fileName) as f:
+    data = json.load(f)
+    objects = data['objects']
+    
     for item in objects:
-        if 'description' in item:
-            description = item['description']
+        #if 'description' in item:
+            #description = item['description']
             ##label = item['labels'][0].replace('-', ' ') 
             #threatstream_severity = item['labels'][1].split("threatstream-severity-")[1]
             #threatstream_confidence = float(item['labels'][2].split("threatstream-confidence-")[1])
             #item['label'] = label
             #item['threatstream_severity'] = threatstream_severity.replace('-', ' ') 
             #item['threatstream_confidence'] = threatstream_confidence
-            item['collection'] = collection.title
-            item['dataSource'] = "Anomali Limo"
+            item['collection'] = fileName
+            item['dataSource'] = "File Upload"
             #item['iType'] = description.split(";")[1].split(":")[1].lstrip()
             #item['state'] = description.split(";")[2].split(":")[1].lstrip()
             #item['name'] = item['pattern'].split("'",1)[1][:-2]
@@ -42,13 +38,12 @@ for collection in collections_limo:
             item['isValidStix'] = "Yes"
             #print(re.search("(?P<url>https?://[^\s]+)", item["name"]).group("url"))
             #ip = re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', item["name"]).group()
-            del item['labels']
+            #del item['labels']
             #del item['pattern']
-            del item['object_marking_refs']
-    objStr = json.dumps(obj)
+            #del item['object_marking_refs']
+            #print(item)
+    objStr = json.dumps(objects)[1:-1]
+    print((objStr))
     #client.push(objStr, 'urn:stix.mitre.org:json:2.1',uri='/services/inbox-a',collection_names=['collection-a'])
     #print(obj)
-    fName = collection.title + ".json"
-    fName.replace(" ", "-")
-    #with open(fName, 'w') as outfile:
-    #    json.dump(obj, outfile)
+
