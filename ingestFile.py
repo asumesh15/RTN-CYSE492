97358700d2c72cd85dcec2eda5388f9a55e586a8
Ajith Sumesh <asumesh15@users.noreply.github.com>
2021-04-21 14:42:06 -0400

    Add files via upload

--- ingestFile.py
+++ ingestFile.py
@@ -42,7 +42,7 @@ with open(fileName) as f:
             #del item['pattern']
             #del item['object_marking_refs']
             #print(item)
-    objStr = json.dumps(objects)[1:-1]
+    objStr = json.dumps(data)
     print((objStr))
     #client.push(objStr, 'urn:stix.mitre.org:json:2.1',uri='/services/inbox-a',collection_names=['collection-a'])
     #print(obj)
